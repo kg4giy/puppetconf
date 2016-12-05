1f7fd791b4a09ec4b4c7174e7cc4809dc0ee3d01
David A. Lane <kg4giy@gmail.com>
2016-12-05 14:28:58 -0500

    Manipulate the firewalls. May need to tweak the cfn access rules too.

--- rpm-install.sh
+++ rpm-install.sh
@@ -7,3 +7,7 @@ sudo yum install -y http://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch
 
 # Installs the application
 sudo yum install -y puppetserver
+
+# Firewall manipulation
+sudo firewall-cmd --permanent --zone=public --add-port=8140/tcp
+sudo firewall-cmd --reload
